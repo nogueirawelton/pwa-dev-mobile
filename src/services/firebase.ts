0f5024b467e78449157767c7929059258e00454c
Welton Nogueira <welton.nogueira.dev@gmail.com>
2023-09-19 19:56:13 -0300

    Update firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,5 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getDatabase();
 export const auth = getAuth();
