096ce6cae2b71c6b050afcdd8e46c8e9e070d5e1
Welton <welton@internit.net.br>
2023-10-23 02:59:49 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,8 @@ import "./firebase";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 import { ToastContainer } from "react-toastify";
-import { useEffect } from "react";
 
 export function App() {
-  useEffect(() => {
-    navigator.serviceWorker
-      .register("/aux-sw.js", {
-        type: "module",
-      })
-      .then(function (registration) {
-        console.log("Service worker  registrado com sucesso:", registration);
-      });
-  }, []);
-
   return (
     <>
       <BrowserRouter>
