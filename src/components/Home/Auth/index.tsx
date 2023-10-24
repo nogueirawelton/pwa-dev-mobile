ddad8d801f4761807163971abfba364bd57128c9
Welton <welton@internit.net.br>
2023-10-24 17:13:26 -0300

    .

--- src/components/Home/Auth/index.tsx
+++ src/components/Home/Auth/index.tsx
@@ -56,9 +56,9 @@ export function Auth() {
       }
     }
 
-    if (!window.matchMedia("(display-mode: standalone)").matches) {
-      return;
-    }
+    // if (!window.matchMedia("(display-mode: standalone)").matches) {
+    //   return;
+    // }
 
     mount();
   }, []);
